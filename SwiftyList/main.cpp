d2a1c75616ce91fb9a86167fe094aea89f3f51a8
aleksandrdremov <dremov.me@gmail.com>
2020-10-30 11:31:38 +0300

    Initial Commit

---
+++ SwiftyList/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SwiftyList
+//
+//  Created by Александр Дремов on 30.10.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
